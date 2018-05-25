3ac1429b501d21d413ea20374d0da8eb915f9e20
sandyirani <31449386+sandyirani@users.noreply.github.com>
2018-05-25 16:15:07 -0700

    Commit

--- cycle.jl
+++ cycle.jl
@@ -58,91 +58,42 @@ for i=1:n
   A[i][1,iseven(i) ? 2 : 1,1] = 1.0
 end
 
-HLR = [zeros(1,1) for i=1:n]	# Initialize to avoid errors on firs sweep
-m = 3
-for swp = 0:10
-  m = round(Int64,1.3*m)
-  for ii=-n+1:n-1		# if negative, going right to left
-    ii == 0 && continue
-    i = abs(ii)
-    toright = ii > 0
-
-    println("\n sweep, i, dir, m = $swp, $i, ",toright ? "to right" : "to left"," $m")
-
-    dleft = size(A[i],1)
-    alpha = dleft * 2
-    dright = size(A[i+1],3)
-    beta = 2 * dright
-    onesite = eye(2)
-
-    HL = zeros(dleft,2,dleft,2)
-    HR = zeros(2,dright,2,dright)
-    if i > 1
-      Aim1 = A[i-1]
-      @tensor begin
-        HL[a,si,ap,sip] := Htwosite[sim1,si,sim1p,sip] * Aim1[b,sim1,a] * Aim1[b,sim1p,ap]
-      end
-      i > 2 && ( HL += JK4(HLR[i-1],onesite) )
-    end
-    HL = reshape(HL,alpha,alpha)
-    if i < n-1
-      Ai2 = A[i+2]
-      @tensor begin
-        HR[si1,b,si1p,bp] := Htwosite[si1,si2,si1p,si2p] * Ai2[b,si2,a] * Ai2[bp,si2p,a]
-      end
-      i < n-2 && (HR += JK4(onesite,HLR[i+2]) )
-    end
-    HR = reshape(HR,beta,beta)
-
-    Oleft =  Any[JK(eye(dleft),sz), 0.5*JK(eye(dleft),sp), 0.5*JK(eye(dleft),sm)]
-    Oright = Any[JK(sz,eye(dright)),JK(sm,eye(dright)),JK(sp,eye(dright))]
-
-    Ai = A[i]
-    Ai1 = A[i+1]
-    @tensor begin
-      AA[a,b,d,e] := Ai[a,b,c] * Ai1[c,d,e]
-    end
-
-    #  Inefficient implementation:  m^4   Ham construction
-    Ham = zeros(alpha*beta,alpha*beta)
-    for j=1:length(Oleft)
-      Ham += JK(reshape(Oleft[j],alpha,alpha),reshape(Oright[j],beta,beta))
-    end
-    if i > 1
-      Ham += JK(HL,eye(beta))
-    end
-    if i < n-1
-      Ham += JK(eye(alpha),HR)
-    end
-    bigH = reshape(Ham,alpha*beta,alpha*beta)
-    bigH = 0.5 * (bigH + bigH')
-    evn = eigs(bigH;nev=1, which=:SR,ritzvec=true,v0=reshape(AA,alpha*beta))
-    @show evn[1]
-    @show size(evn[2])
-    gr = evn[2][:,1]
-
-    AA = reshape(gr,dleft,2,2,dright)
-
-    (A[i],A[i+1],trunc) = dosvd4(AA,m,toright)
-    @show trunc
-    if toright
-      if 1 < i < n-1
-        (i1,i2,i3) = size(A[i])
-        Ai2 = reshape(A[i],i1*i2,i3)
-        @tensor begin
-          hlri[b,bp] := HL[a,ap] * Ai2[a,b] * Ai2[ap,bp]
-        end
-        HLR[i] = hlri
-      end
-    else
-      if 1 < i < n-1
-        (i1,i2,i3) = size(A[i+1])
-        Ai12 = reshape(A[i+1],i1,i2*i3)
-        @tensor begin
-          hlri1[a,ap] := HR[b,bp] * Ai12[a,b] * Ai12[ap,bp]
-        end
-        HLR[i+1] = hlri1
+function mainLoop()
+  numIters = [1000,2000,8000]
+  #numIters = [100,200,1000]
+  taus = [.1,.01,.001]
+  for stage = 1:3
+    tau = taus[stage]
+    numIter = numIters[stage]
+    for iter = 1:numIter
+      taugate = reshape(expm(-tau * reshape(Htwosite,4,4)),2,2,2,2)
+      #println("\n iteration = $iter")
+      for j = 1:N
+        applyGateAndTrim(j,taugate)
       end
     end
+    println("\n End of stage $stage")
   end
+end
+
+function normEnv(j)
+  nHalf = Int64(ceil(n/2))
+  mid = mod(j+nHalf,n)+1
+end
+
+function calcEnv(l,r,toRight)
+  ld = size(A[l],1)
+  E = eye(ld)
+  num = r >= l? l-r+1: n-l+r+1
+  curr = l
+  for k = 1:num
+    Nc = N[curr]
+    Ncp = Nc'
+    @tensor begin
+      Enew[c,d] := E[a,b]*Ncp[a,p,c]*Nc[b,p,d]
+    end
+    E = Enew
+    curr = mod(curr,n)+1
+  end
+  return(E)
 end
