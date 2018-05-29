9aa7de79f408ebbb363e3dbed8f1b88fd95498ed
sandyirani <31449386+sandyirani@users.noreply.github.com>
2018-05-29 12:18:46 -0700

    Commit

--- cycle.jl
+++ cycle.jl
@@ -84,6 +84,30 @@ function mainLoop()
   @show(calcEnergy())
 end
 
+function mainLoopLine()
+  numIters = [1000,2000,8000]
+  #numIters = [100,200,1000]
+  taus = [.1,.01,.001]
+  @show(calcOverlapCycle(A,A))
+  for stage = 1:3
+    tau = taus[stage]
+    numIter = numIters[stage]
+    for iter = 1:numIter
+      taugate = reshape(expm(-tau * reshape(Htwosite,4,4)),2,2,2,2)
+      #println("\n iteration = $iter")
+      for k = 1:n-1
+        j = Int64(ceil(rand()*(n-1)))
+        normEnvLine(j)
+        jp1 = mod(j,n)+1
+        (A[j],A[jp1]) = applyGateAndTrim(A[j],A[jp1],taugate)
+      end
+    end
+    println("\n End of stage $stage")
+    @show(calcOverlapCycle(A,A))
+  end
+  @show(calcEnergy())
+end
+
 function normEnv(j)
 
   nHalf = Int64(ceil(n/2))
@@ -125,6 +149,46 @@ function normEnv(j)
   Ajp1 = reshape(A[jp1],s[1]*s[2],s[3])
   Ajp1 = Ajp1*U*diagm(dSqrt)
   A[jp1] = reshape(Ajp1,s[1],s[2],length(d))
+
+end
+
+function normEnvLine(j)
+
+  if (j > 1)
+    Eleft = calcEnv(1,j-1,true)
+    Eleft = 0.5*(Eleft+Eleft')
+    F = eigfact(Eleft)
+    d = F[:values]
+    U = F[:vectors]
+    (d,U) = cleanEigs(d,U)
+    dSqrt = sqrt.(d)
+    s = size(A[j-1])
+    Ajm1 = reshape(A[j-1],s[1]*s[2],s[3])
+    Ajm1 = Ajm1*U*diagm(inv.(dSqrt))
+    A[j-1] = reshape(Ajm1,s[1],s[2],length(d))
+    s = size(A[j])
+    Aj = reshape(A[j],s[1],s[2]*s[3])
+    Aj = diagm(dSqrt)*U'*Aj
+    A[j] = reshape(Aj,length(d),s[2],s[3])
+  end
+
+  if (j < n-1)
+    Eright = calcEnv(j+2,n,false)
+    Eright = 0.5*(Eright+Eright')
+    F = eigfact(Eright)
+    d = F[:values]
+    U = F[:vectors]
+    (d,U) = cleanEigs(d,U)
+    dSqrt = sqrt.(d)
+    s = size(A[jp2])
+    Ajp2 = reshape(A[j+2],s[1],s[2]*s[3])
+    Ajp2 = diagm(inv.(dSqrt))*U'*Ajp2
+    A[j+2] = reshape(Ajp2,length(d),s[2],s[3])
+    s = size(A[j+1])
+    Ajp1 = reshape(A[j+1],s[1]*s[2],s[3])
+    Ajp1 = Ajp1*U*diagm(dSqrt)
+    A[j+1] = reshape(Ajp1,s[1],s[2],length(d))
+  end
 
 end
 
